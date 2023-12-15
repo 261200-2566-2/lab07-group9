dcb927a262aedf31bdfdf061c07f2c6b17861411
SUPATKN <yacht1400@gmail.com>
2023-12-16 01:53:54 +0700

    add addAll clear

---
+++ src/Lab07.java
@@ -0,0 +1,14 @@
+import java.util.Collection;
+
+public class Lab07 {
+    public static void main(String[] args) {
+        Set<Integer> s = new Set<>();
+        Set<Integer> s2 = new Set<>();
+        s.add(1);
+
+        s2.add(2);
+
+        s2.addAll(s);
+
+    }
+}
---
+++ src/Set.java
@@ -0,0 +1,36 @@
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Objects;
+
+
+public class Set<T>  {
+    private final HashMap<T, Object> map;
+    private final Object dummy = new Object();
+
+    public Set(){
+        map = new HashMap<>();
+    }
+
+    public boolean add(T value){
+        return map.put(value,dummy) == null;
+        //ถ้ามีตัวยังไม่มีตัวจะเพิ่ม(== null) return true แต่ถ้ามีแล้ว (!= null) return false
+    }
+
+    //mean U
+    public boolean addAll(Set<T> s2){
+        boolean result = false;
+        for (T element: s2.map.keySet()) {
+            //keySet for call all element in s2
+            if(add(element)){
+                //add element s2 to this Set
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    public void clear(){
+        map.clear();
+    }
+}
